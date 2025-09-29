967c45838663c401e28c53a8a280fc288de2cf9a
Maniakalochka <prochorwolochoff@gmail.com>
2025-09-29 13:50:33 +0300

    add initial database session management and configuration

---
+++ src/db/session.py
@@ -0,0 +1,26 @@
+from typing import AsyncGenerator
+
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+from sqlalchemy.pool import NullPool
+
+from core.config import settings
+
+DB_URL = settings.AUTH_DB_URL
+DATABASE_PARAMS = {"poolclass": NullPool}
+
+engine = create_async_engine(url=DB_URL, echo=False, **DATABASE_PARAMS)
+
+SessionLocal = async_sessionmaker(
+    bind=engine,
+    class_=AsyncSession,
+    expire_on_commit=False,
+)
+
+
+async def get_async_session() -> AsyncGenerator[AsyncSession]:
+    async with SessionLocal() as session:
+        try:
+            yield session
+        except Exception as e:
+            await session.rollback()
+            raise e
